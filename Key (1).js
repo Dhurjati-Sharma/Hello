0713ba2921d1165fefd1817047a9e00cc1fa95f7
Dhurjati-Sharma <85036440+Dhurjati-Sharma@users.noreply.github.com>
2021-08-23 22:04:59 +0530

    Add files via upload

---
+++ Key (1).js
@@ -0,0 +1,3 @@
+window.onload = function jj(){
+if(EnterYourkey == key){location.href='index.html'}else{}
+document.getElementById("helloworofofoofofofffnubvvbtvhbvhtfbtt").innerHTML = EnterYourkey;}
